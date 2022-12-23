807c786235893e555961e284db01873468d5dbc8
kangjiwon <86946783+kanggeowon@users.noreply.github.com>
2022-12-23 11:30:58 +0900

    Delete myproject directory

--- myproject/pybo/forms.py
+++
@@ -1,9 +0,0 @@
-# myproject/pybo/forms.py
-
-from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField
-from wtforms.validators import DataRequired
-
-class QuestionForm(FlaskForm):
-    subject = StringField('제목', validators=[DataRequired()])
-    content = TextAreaField('내용', validators=[DataRequired()])
