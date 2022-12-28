63aabd992f698bd8fc84c290426251cd358024eb
beom98 <osr0912@gmail.com>
2022-12-28 12:48:44 +0900

    12:48

--- myproject/sesac/static/js/user.post.edit.js
+++ myproject/sesac/static/js/user.post.edit.js
@@ -7,7 +7,7 @@ function enterFunc(event, id, focus){
         if(data.value.length==0){
             event.preventDefault()
             Swal.fire({
-                icon: 'waring',                         // Alert 타입
+                icon: 'warning',                         // Alert 타입
                 title: '경고',         // Alert 제목
                 text: `${id}은 필수 입력 항목입니다.`,  // Alert 내용
             });
@@ -22,13 +22,13 @@ function btnClick(event){
     i=0
     clas = document.querySelectorAll(".form-control")
     for(i=0; i<=6; i++){
-    if(clas[i].value.length==0){
-        Swal.fire({
-            icon: 'error',                         // Alert 타입
-            title: '경고',         // Alert 제목
-            text: "내용 다 채워주세요",  // Alert 내용
-        });
-        event.preventDefault()
+        if(clas[i].value.length==0){
+            Swal.fire({
+                icon: 'error',                         // Alert 타입
+                title: '경고',         // Alert 제목
+                text: "내용 다 채워주세요",  // Alert 내용
+            });
+            event.preventDefault()
         }
     }
 }
